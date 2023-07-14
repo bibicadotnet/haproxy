847172bc13b6e422bd9e905aac9b741594e61bdb
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-14 21:47:03 +0700

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -1,8 +1,6 @@
+#!/bin/bash
+# setup Webinoly php 7.4
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/setup.sh -O setup.sh && sudo chmod +x setup.sh && sudo ./setup.sh
-# setup bypass Oracle 15% RAM
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bypass_oracle.sh -O /usr/local/bin/bypass_oracle.sh
-chmod +x /usr/local/bin/bypass_oracle.sh
-nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &
 
 # setup wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -13,9 +11,17 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 # setup rclone
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
-# setup crontab
+# Bypass Oracle VM.Standard.A1.Flex
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/NeverIdle-Oracle/master/VM.Standard.A1.Flex.4GB.RAM.sh -O /usr/local/bin/bypass_oracle.sh
+chmod +x /usr/local/bin/bypass_oracle.sh
+nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &
+
+# setup crontab cho wp_cron and simply-static
+mkdir -p /var/www/bibica.net/static-files-temp
+chmod 777 /var/www/bibica.net/static-files-temp
 crontab -l > simply-static
-echo "*/1 * * * * curl https://haproxy.tech/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> simply-static
+echo "0 3 * * * /usr/local/bin/wp --path='/var/www/bibica.net/htdocs' simply-static run --allow-root" >> simply-static
+echo "*/1 * * * * curl https://bibica.net/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> simply-static
 echo "@reboot nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &" >> simply-static
 crontab simply-static
 
