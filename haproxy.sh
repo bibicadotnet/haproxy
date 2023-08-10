5ffbcc744e5cad16c266b2fdda640a592dd50b89
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-08-10 22:14:05 +0700

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -17,11 +17,11 @@ chmod +x /usr/local/bin/bypass_oracle.sh
 nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &
 
 # setup crontab cho wp_cron and simply-static
-mkdir -p /var/www/bibica.net/static-files-temp
-chmod 777 /var/www/bibica.net/static-files-temp
+mkdir -p /var/www/haproxy.tech/static-files-temp
+chmod 777 /var/www/haproxy.tech/static-files-temp
 crontab -l > simply-static
-echo "0 3 * * * /usr/local/bin/wp --path='/var/www/bibica.net/htdocs' simply-static run --allow-root" >> simply-static
-echo "*/1 * * * * curl https://bibica.net/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> simply-static
+echo "0 3 * * * /usr/local/bin/wp --path='/var/www/haproxy.tech/htdocs' simply-static run --allow-root" >> simply-static
+echo "*/1 * * * * curl https://haproxy.tech/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> simply-static
 echo "@reboot nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &" >> simply-static
 crontab simply-static
 
